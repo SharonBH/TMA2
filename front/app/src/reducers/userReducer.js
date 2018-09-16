643fc7837cab61e8cd16af954995668c3eea262c
unknown <kira@beehivebi.com>
2018-09-16 16:08:58 +0300

    store

--- front/app/src/reducers/userReducer.js
+++ front/app/src/reducers/userReducer.js
@@ -1,10 +1,14 @@
-const InitialState = {}
+const InitialState = {
+    some: '12123132'
+}
 
 
 const userReducer = (state = InitialState, action) => {
-    switch (action.type) {
+    // switch (action.type) {
+       
         
-    }
+    // }
+    console.log('reducer test ', state.some)
 }
 
 
