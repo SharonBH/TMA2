75dcc6e02e8c1782bb67501112dbc2b69b53a533
liber.roi <roi@beehivebi.com>
2018-10-09 18:09:14 +0300

    app api

--- front/app/src/App.js
+++ front/app/src/App.js
@@ -8,6 +8,8 @@ import { connect } from 'react-redux';
 import { withCookies } from 'react-cookie';
 import ConfirmMessage from './components/UI/ConfirmMessage';
 import TopNavigation  from './containers/TopNavigation/TopNavigation';
+import { appCallTakeAllTournaments, appCallTakeAllEvents } from './actions/GamesApi';
+import { appCallTakeAllUsers } from './actions/Api';
 
 class App extends Component {
   spinner = () => {
@@ -17,28 +19,46 @@ class App extends Component {
         return null
     }
 }
-  render() {
-    return (
-      <div className={classes.App}>
-        
-        {this.spinner()}
-        {history.location.pathname === '/' || history.location.pathname === '/register' ?  '' : <TopNavigation/>}
-        {history.location.pathname === '/' || history.location.pathname === '/register' ?  '' : <Nav />}
-        <MainPage />
-        {this.props.signOutConfirmMessage ? <ConfirmMessage headline='sign out' user=''/> : null}
-      </div>
-    );
-  }
+
+componentWillMount() {
+    this.props.appCallTakeAllTournaments()
+    this.props.appCallTakeAllEvents()
+    if(this.props.currentUser.role === 'Admin') {
+        this.props.appCallTakeAllUsers()
+    }
+}
+
+    render() {
+        return (
+            <div className={classes.App}>
+                
+                {this.spinner()}
+                {history.location.pathname === '/' || history.location.pathname === '/register' ?  '' : <TopNavigation/>}
+                {history.location.pathname === '/' || history.location.pathname === '/register' ?  '' : <Nav />}
+                <MainPage />
+                {this.props.signOutConfirmMessage ? <ConfirmMessage headline='sign out' user=''/> : null}
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
     return {
         toggleSpinner: state.sharedReducer.toggleSpinner,
         signOutConfirmMessage: state.confirmMessageReducer.signOutConfirmMessage,
+        currentUser: state.userReducer.currentUser
     }
 }
 
-export default withCookies(connect(mapStateToProps)(App));
+const mapDispatchToProps = dispatch => {
+    return {
+        appCallTakeAllTournaments: payload => dispatch(appCallTakeAllTournaments(payload)),
+        appCallTakeAllEvents: payload => dispatch(appCallTakeAllEvents(payload)),
+        appCallTakeAllUsers: payload => dispatch(appCallTakeAllUsers(payload)),
+    }
+}
+
+export default withCookies(connect(mapStateToProps, mapDispatchToProps)(App));
 
 
 
