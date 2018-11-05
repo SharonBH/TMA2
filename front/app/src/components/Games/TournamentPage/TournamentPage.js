f1083fb31919ba2c2d19b9796e61e00d056eaef0
unknown <kira@beehivebi.com>
2018-11-05 14:44:27 +0200

    teurn page

--- front/app/src/components/Games/TournamentPage/TournamentPage.js
+++ front/app/src/components/Games/TournamentPage/TournamentPage.js
@@ -59,7 +59,10 @@ export class TournamentPage extends Component {
         if(this.props.allTournsList.length === 0 || this.props.allTournsList === undefined) {
             this.props.takeAllTournaments()
         }
-        // this.props.goToTournPageRequest(TourId)
+        if(this.props.tournById === null){
+            this.props.goToTournPageRequest(TourId)
+        }
+        
         
     }
     componentDidMount(){
