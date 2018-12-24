502ddfacb26195d5844c3ff040d9ff70efc6342a
Shay <Shay@beehivebi.com>
2018-12-24 15:44:26 +0200

    adding debugger

--- front/app/src/App.js
+++ front/app/src/App.js
@@ -24,11 +24,12 @@ class App extends Component {
   };
 
     render() {
+        debugger;
         const isUserNotInSession =  sessionStorage.getItem('session') === null;
         console.log("isUserNotInSession, " + isUserNotInSession);
         console.log("history.location.pathname, " + history.location.pathname);
         const divRegister = history.location.pathname === '/register' ?  <div className={classes.App}>{this.spinner()}<Register headline={REGISTER}/></div> : <div className={classes.App}>{this.spinner()}<Login /></div>;
-        console.log("divRegister, " + divRegister);
+        console.log("divRegister, " + divRegister.toString());
         const divToShow = isUserNotInSession ? divRegister :
             <div className={classes.App}>
                 {this.spinner()}
