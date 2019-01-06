77103e83d78cba94a71569f9aab39bbca03a62a7
amit <amit@beehivebi.com>
2019-01-06 15:29:42 +0200

    url api from localhost

--- front/app/src/actions/Api.js
+++ front/app/src/actions/Api.js
@@ -15,10 +15,17 @@ import {
 } from './index';
 import {EDIT, USER_ROLE_ADMIN, YOUR_PROFILE} from '../configuration/config'
 
+
+
 // const cors ='https://cors-anywhere.herokuapp.com/'
-const cors =''
-const url = 'https://tma-api.azurewebsites.net/'
-//const url = 'https://localhost:44336/'
+const cors = ''
+const location = window.location.toString();
+let url = 'https://tma-api.azurewebsites.net/';
+if (location.indexOf("localhost") > 1) {
+    url = 'https://localhost:44336/'
+}
+
+
 //chrome.exe --user-data-dir="C:/Chrome dev session" --disable-web-security
 
 
