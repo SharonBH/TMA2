4d2c4578e1d7357418e9b6dd4feb247212ec7448
Sharon Ben Haim <sharon@beehivebi.com>
2019-05-05 12:34:13 +0300

    api remote url

--- front/app/src/actions/GamesApi.js
+++ front/app/src/actions/GamesApi.js
@@ -34,7 +34,7 @@ import {
 // const cors = 'https://cors-anywhere.herokuapp.com/'
 const cors = '';
 const location = window.location.toString();
-let url = 'https://localhost:44336/';
+let url = 'https://tma-api.azurewebsites.net/';
 /* if (location.indexOf("localhost") > 1) {
     url = 'https://localhost:44336/'
 } */
